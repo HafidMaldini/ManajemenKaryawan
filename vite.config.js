222798bbaf40705a602c2f9ecb9c0830b55498a1
HafidMaldini <maldiniyusan17@gmail.com>
2025-02-19 19:20:55 +0700

    tes

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,11 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
-
-
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: true, // Izinkan akses dari luar
-    port: 5173, // Pastikan sesuai dengan Railway
-  },
-  build: {
-    outDir: 'dist', // Pastikan output berada di dist
-  }
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
 });
